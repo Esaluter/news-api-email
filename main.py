f320a85acb6ffef7c74ad8e170394d5eb48f686f
Esaluter <esaluter@mail.ru>
2023-11-28 19:15:37 +0500

    add message and send email

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import requests
+from send_email import send_email
 
 api_key = "70b766f12e554e58a3efacb67ce28ed6"
 url = "https://newsapi.org/v2/everything?"\
@@ -12,6 +13,13 @@ request = requests.get(url)
 content = request.json()
 
 #acces article title and descriptions
-for article in content['articles']:
-    print(article["title"])
-    print(article["description"])
+body = ''
+for article in content["articles"]:
+    if article["title"] is not None:
+        body = body + article["title"] + "\n" + article["description"] + 2*"\n"
+    #print(article["title"])
+    #print(article["description"])
+        #message = f'Привет! Лови немного новостей {article["title"]} {article["description"]}'  
+
+body = body.encode("UTF-8")
+send_email(body)
