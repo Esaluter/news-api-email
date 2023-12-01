7d78050c2e59b3197680e1dc2f8cd3ca15f5d733
Esaluter <esaluter@mail.ru>
2023-12-01 17:28:03 +0500

    Addnew things

--- main.py
+++ main.py
@@ -3,8 +3,11 @@ from send_email import send_email
 
 api_key = "70b766f12e554e58a3efacb67ce28ed6"
 url = "https://newsapi.org/v2/everything?"\
-        "q=tesla&from=2023-10-28&sortBy=publishe"\
-        "dAt&apiKey=70b766f12e554e58a3efacb67ce28ed6"
+    "q=tesla&"\
+    "from=2023-11-01&" \
+    "sortBy=publishedAt&"\
+    "apiKey=70b766f12e554e58a3efacb67ce28ed6&"\
+    "language=ru"
 
 #make request
 request = requests.get(url)
@@ -14,12 +17,12 @@ content = request.json()
 
 #acces article title and descriptions
 body = ''
-for article in content["articles"]:
+for article in content["articles"][:20]:
     if article["title"] is not None:
-        body = body + article["title"] + "\n" + article["description"] + 2*"\n"
-    #print(article["title"])
-    #print(article["description"])
-        #message = f'Привет! Лови немного новостей {article["title"]} {article["description"]}'  
+        body = body + article["title"] + "\n" \
+        + article["description"] \
+        + "\n" + article["url"] + 2*"\n" 
+      #"Subject: Новости" + "\n"  + 
 
 body = body.encode("UTF-8")
 send_email(body)
